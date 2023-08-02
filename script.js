a8d5b5b27641b44425f41c6d0e0a1e03fa700a23
Githubminhkhoi <133839066+Githubminhkhoi@users.noreply.github.com>
2023-08-02 09:44:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,476 @@
+const questions = [
+//Topic Linguistics
+  {
+    question: ' the act of separating people or things into different groups?',
+    answers: ['crowd', 'distinction', 'gravity'],
+    answer: 'distinction'
+},
+{
+  question: ' a movement that you make with your hands, your head or your face to show a particular meaning?',
+  answers: ['gesture', 'attitude', 'function'],
+  answer: 'gesture'
+},
+{
+    question: ' the process by which knowledge and understanding is developed in the mind?',
+    answers: ['relation  ', 'refference', 'cognition'],
+    answer: ' cognition '
+},
+{
+    question: ' a linguistic variety peculiar to a particular geographical region or used by members of a specific social class?',
+    answers: ['language ', 'dialect', 'odinary'],
+    answer: ' dialect '
+},
+{
+    question: 'people who describe the rule-governed structures of languages?',
+    answers: [' philosopher ', 'scientist', ' linguist '],
+    answer: ' linguist '
+},
+
+{
+    question: ' to develop gradually, especially from a simple to a more complicated form; to develop something in this way?',
+    answers: ['envolve ', 'evolve', ' involve '],
+    answer: ' evolve '
+},
+{
+    question: ' to make a quick and clever remark?',
+    answers: ['quip ', 'smart', ' persiflage '],
+    answer: ' quip '
+},
+{
+    question: ' to become or make it become difficult to recognize a clear difference between things?',
+    answers: ['blur ', 'fog', ' illution '],
+    answer: ' blur '
+},
+{
+    question: ' the features of a problem or situation that are difficult to understand?',
+    answers: ['politics', 'complexities', 'imply '],
+    answer: ' complexities '
+},
+//Topic Earth Sience
+  {
+    question: 'A mixture of gases that surrounds a planet or a star',
+    answers: ['Absorb', 'Atmosphere', 'Variability'],
+    answer: 'Atmosphere'
+},
+{
+    question: 'The fact of something being likely to vary',
+    answers: ['Emission', 'Potential', 'Variability'],
+    answer: 'Variability'
+},
+{
+    question: 'Very suddenly and to a very great and often surprising degree',
+    answers: ['Emit', 'Dramatically', 'Retain'],
+    answer: 'Dramatically'
+},
+{
+    question: 'A change in opinion, mood, policy, etc',
+    answers: ['Humid', 'Absorb', 'Decay'],
+    answer: 'Shift'
+},
+{
+    question: 'To send out something such as light, heat, sound, gas, etc',
+    answers: ['Emit', 'Emission', 'Output'],
+    answer: 'Emit'
+},
+{
+    question: 'Gas, etc. that is sent out into the air',
+    answers: ['Emit', 'Output', 'Emission'],
+    answer: 'Emission'
+},
+{
+    question: 'The possibility of something happening or being developed or used',
+    answers: ['Potential', 'Predictable', 'Ensure'],
+    answer: 'Potential'
+},
+{
+    question: 'To keep something; to continue to have something',
+    answers: ['Absorb', 'Retain', 'Shift'],
+    answer: 'Retain'
+},
+{
+    question: 'To take in and keep heat, light, energy, sound, etc. instead of reflecting it',
+    answers: ['Humid ', 'Thermal', 'Absorb'],
+    answer: 'Absorb'
+},
+//Topic: Bussiness ethics
+{
+  question: 'The company’s code of conduct is designed to trigger ethical decision-making among employees.',
+  answers: ['Trigger', 'Shoot', 'Fire'],
+  answer: 'Trigger'
+},
+{
+  question: 'The company’s massive investment in ethical business practices has helped it build a reputation for being a responsible corporate citizen',
+  answers: ['Small', 'Large', 'Massive'],
+  answer: 'Massive'
+},
+{
+  question: 'The company’s decline in ethical business practices has led to a loss of customer trust',
+  answers: ['Growth', 'Bottom', 'Decline'],
+  answer: 'Decline'
+},
+{
+  question: 'The standards of ethical business practices can vary from country to country.',
+  answers: ['Fix', 'Vary', 'Destroy'],
+  answer: 'Vary'
+},
+{
+  question: 'The boundary for ethical business practices can vary depending on the industry.',
+  answers: ['Boundary', 'Temperature', 'Concentration'],
+  answer: 'Boundary'
+},
+{
+  question: 'The company promote people should wear a masks when working in industry.',
+  answers: ['Protect', 'Promote', 'Provide'],
+  answer: 'Promote'
+},
+{
+  question: 'Factories pollute the atmosphere',
+  answers: ['Pollution', 'Gases', 'Atmosphere'],
+  answer: 'Atmosphere'
+},
+{
+  question: 'Ultimately, Vietnam economy has also grown about everything',
+  answers: ['Begin', 'Ever', 'Ultimately, '],
+  answer: 'Ultimately'
+},
+{
+  question: ' Supplier is very important in business',
+  answers: [' Supplier', 'Church', 'Restaurant'],
+  answer: 'Supplier'
+},
+//Topic implication and inference
+{
+  question: ' to suggest that something is true or that you feel or think something, without saying so directly',
+  answers: [' Imply ', ' Facilitate ', ' Variability '],
+  answer: ' Imply '
+},
+{
+  question: ' something that is suggested or indirectly stated ',
+  answers: [' Retirement ', 'Potential', ' Implication '],
+  answer: ' Implication '
+},
+{
+  question: ' If something is predictable, you know in advance that it will happen or what it will be like? ',
+  answers: ['Emit', ' Predictable ', ' Defect '],
+  answer: ' Predictable '
+},
+{
+  question: ' To assume ',
+  answers: [' Equator ', ' Presume ', 'Decay'],
+  answer: ' Presume '
+},
+{
+  question: ' A complete range of something, going from one extreme to the opposite ',
+  answers: ['Emit', ' Spectrum ', ' Uncertainty '],
+  answer: ' Uncertainty '
+},
+{
+  question: ' To make something worse, especially a disease or problem ',
+  answers: [' Exacerbate ', ' Vulnerable ', 'Emission'],
+  answer: ' Exacerbate '
+},
+{
+  question: ' A continuous decrease in the number, value, quality, etc. of something ',
+  answers: [' Decline ', ' Uninhabitable ', 'Ensure'],
+  answer: ' Decline '
+},
+{
+  question: ' connected with studying things to find out how similar or different they are ',
+  answers: [' Blur ', 'Retain', ' Comparative '],
+  answer: ' Comparative '
+},
+{
+  question: '  A thing, situation, etc. that is different from what is normal or expected ',
+  answers: [' Alternate ', ' Anomaly ', ' Absorb '],
+  answer: ' Anomaly '
+},
+{
+  question: ' The amount of something that a person, a machine, etc. produces ?',
+  answers: ['Output', 'Expand', 'Volume'],
+  answer: 'Output'
+},
+{
+  question : ' Any animal that gives birth to live young, not eggs, and feeds its young on milk. Cows, humans and whales are all mammals ? ',
+  answers :[ 'Relevant', ' Capacity ', ' Mammal'],
+  answer : 'Mammal'
+},
+{
+  question: ' Connected with managing a business or an organization, and with making plans and decisions ?',
+  answers: [' Repercussion ', ' Executive ', ' Taboo'],
+  answer : ' Executive ' 
+},
+{
+  question: 'A plan or program designed to improve a situation or address a problem?',
+  answers: [ ' Violation ','Primary ','Initiative '],
+  answer:' Initiative' 
+}, 
+{
+  question: 'To fill something somebody completely with something so that it is impossible or useless to add any more?',
+  answers: [ ' Saturate ', ' Trigger','Intense'],
+  answer: 'Saturate'
+}, 
+{
+  question : ' A complete range of something, going from one extreme to the opposite ',
+  answers : [ ' Innovative ','Spectrum','Stability'],
+  answer : 'Spectrum'
+},
+{
+  question : 'A copy of something, usually smaller than the original object',
+  answers : ['Model','Tissue',' Expand '],
+  answer: ' Model'
+},
+{
+  question :' To describe or show the qualities of somebody/something in a particular way?',
+  answers : [ 'Equator',' Characterize ',' Humid'],
+  answer : ' Characterize '
+},
+{
+  question: 'If something is predictable, you know in advance that it will happen or what it will be like ',
+  answers : [ ' Vary ',' Predictable ','Decay' ],
+  answer : ' Predictable '
+},
+  // Add more questions here...
+];
+
+const healthBar1 = document.getElementById("health1");
+const healthBar2 = document.getElementById("health2");
+let health1 = 100; // Initial health for character 1
+let health2 = 100; // Initial health for character 2
+let atk1 = 30; // Attack stat for character 1
+let atk2 = 30; // Attack stat for character 2
+let def1 = 10; // Defense stat for character 1
+let def2 = 10; // Defense stat for character 2
+let round = 0
+let currentPlayer = 2;
+
+function decreaseHealth() {
+  if (currentPlayer === 1) {
+    const damageTaken = atk2 - def1; // Calculate the damage taken by player 1
+    health1 -= damageTaken;
+    console.log(`Player 1 Health: ${health1}`);
+    document.getElementById('player1-HP').innerText = `Player 1: ${health1} (Atk: ${atk1}, Def: ${def1})`;
+    if (health1 <= 0) {
+      setTimeout(() => {
+        showGameOver();
+      }, 5000);
+    }
+  } else if (currentPlayer === 2) {
+    const damageTaken = atk1 - def2; // Calculate the damage taken by player 2
+    health2 -= damageTaken;
+    console.log(`Player 2 Health: ${health2}`);
+    document.getElementById('player2-HP').innerText = `Player 2: ${health2} (Atk: ${atk2}, Def: ${def2})`;
+    if (health2 <= 0) {
+      setTimeout(() => {
+        showGameOver();
+      }, 5000);
+    }
+  }
+  updateHealthBar();
+}
+
+function updateHealthBar() {
+  console.log(`Updating health bars - Player 1: ${health1}, Player 2: ${health2}`);
+  healthBar1.style.width = health1 + "%";
+  healthBar2.style.width = health2 + "%";
+}
+
+
+
+function getRandomQuestion() {
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  return questions[randomIndex];
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function showQuestion() {
+
+  // Get the clicked cell
+  const clickedCell = event.target;
+
+  // Add the "selected" class to the clicked cell
+  clickedCell.classList.add("selected");
+  clickedCell.removeAttribute("onclick");
+  const questionContainer = document.getElementById('notification-container');
+  questionContainer.innerHTML = ''; // Clear previous question and answers
+
+  const question = getRandomQuestion();
+  const { question: questionText, answers, answer } = question;
+  const questionElement = document.createElement('div');
+
+  questionElement.className = 'question';
+  questionElement.innerText = questionText;
+  questionContainer.appendChild(questionElement);
+
+  const shuffledAnswers = shuffleArray(answers);
+
+  for (const answerOption of shuffledAnswers) {
+    const answerElement = document.createElement('div');
+    answerElement.className = 'answer';
+    answerElement.innerText = answerOption;
+    answerElement.onclick = () => showResult(answerOption === answer);
+    questionContainer.appendChild(answerElement);
+  }
+
+  // Switch to the other player
+  currentPlayer = currentPlayer === 1 ? 2 : 1;
+  round++;
+  document.getElementById('round').innerText = `Round: ${round}`;
+  document.getElementById('player-turn').innerText = `Player turn: ${currentPlayer}`;
+}
+
+function showResult(isCorrect) {
+
+  const resultContainer = document.getElementById('notification-container');
+  resultContainer.innerHTML = '';
+
+  const resultElement = document.createElement('div');
+  resultElement.className = 'result';
+  resultElement.innerText = isCorrect ? 'Correct Plese go to next turn ' : 'Incorrect Plese go to next turn';
+  resultContainer.appendChild(resultElement);
+
+  // Play sound effect
+  const correctSound = document.getElementById('correctSound');
+  const incorrectSound = document.getElementById('incorrectSound');
+  if (isCorrect) {
+    correctSound.play();
+    displayRandomOption();
+    if (currentPlayer === 1) {
+      health2 -= atk1 - def2;
+      updateHealthBar()
+    } else {
+      health1 -= atk2 - def1;
+      updateHealthBar()
+    }
+  } else {
+    incorrectSound.play();
+    decreaseHealth();
+  }
+
+  // Check if any player has health = 0 or max round
+  if (health1 === 0 || health2 === 0 || round >=16) {
+    setTimeout(() => {
+      showGameOver();
+    }, 1000); // Show the game over message after 5 second
+  }
+  
+}
+function resetGame() {
+  
+  // Reset player stats               
+  health1 = 100; // Initial health for character 1
+  health2 = 100; // Initial health for character 2    
+  health1 = 100;
+  health2 = 100;
+  atk1 = 20;
+  atk2 = 20;
+  def1 = 10;
+  def2 = 10;           
+  updateHealthBar();
+  document.getElementById('player1-HP').innerText = `Player 1: ${health1} (Atk: ${atk1}, Def: ${def1})`;
+  document.getElementById('player2-HP').innerText = `Player 2: ${health2} (Atk: ${atk2}, Def: ${def2})`;
+
+  // Reset current player
+  currentPlayer = 2;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+  document.getElementById('player-turn').innerText = 'Player turn: 1';
+  
+  //Reset round
+  round = 0;
+  document.getElementById('round').innerText = `Round: ${round}`;
+
+  // Remove the "selected" class from all cells
+  const cells = document.querySelectorAll('#table td');
+  cells.forEach((cell) => {
+    cell.classList.remove('selected');
+    cell.onclick = showQuestion; // Reassign the onclick handler to showQuestion()
+  });
+}
+
+function showGameOver() {
+  
+  const notificationContainer = document.getElementById('notification-container');
+  notificationContainer.innerHTML = '';
+
+  const gameOverElement = document.createElement('div');
+  gameOverElement.className = 'game-over';
+  
+  if (health1 === health2 && round === 16) {
+    gameOverElement.innerText = 'It\'s a tie!';
+  } else if (health1 > health2 && health1 > 0) {
+    gameOverElement.innerText = 'Player 1 wins!';
+  } else if (health2 > health1 && health2 > 0) {
+    gameOverElement.innerText = 'Player 2 wins!';
+  }
+  notificationContainer.appendChild(gameOverElement);
+  resetGame();
+  
+}
+function displayRandomOption() {
+  const options = ['atk', 'def'];
+  const randomOption = options[Math.floor(Math.random() * options.length)];
+
+  const resultContainer = document.getElementById('notification-container');
+  resultContainer.innerHTML = '';
+
+  const resultElement = document.createElement('div');
+  resultElement.className = 'result';
+  resultElement.innerText = `Random Option: ${randomOption}`;
+  resultContainer.appendChild(resultElement);
+
+  // Roll stats mới sau khi đổi lượt chơi
+  if (currentPlayer === 1) {
+    if (randomOption === 'atk') {
+      atk1+=5;
+    } else {
+      def1+=5;
+    }
+    document.getElementById('player1-HP').innerText = `Player 1: ${health1} (Atk: ${atk1}, Def: ${def1})`;
+  } else if (currentPlayer === 2) {
+    if (randomOption === 'atk') {
+      atk2+=5;
+    } else {
+      def2+=5;
+    }
+    document.getElementById('player2-HP').innerText = `Player 2: ${health2} (Atk: ${atk2}, Def: ${def2})`;
+  }
+}
+
+
+const characterSprite = document.getElementById('character');
+const characterImage = characterSprite.querySelector('img');
+let frameIndex = 0;
+let isAnimating = false;
+const framesPerRow = 4; // Số khung hình trên mỗi hàng của sprite sheet
+const frameWidth = 205; // Đặt chiều rộng của mỗi khung hình (707/4)
+const frameHeight = 200; // Đặt chiều cao của mỗi khung hình (353/2)
+
+function animateCharacter() {
+  if (isAnimating) {
+    // Tính toán vị trí left và top của sprite sheet dựa trên frameIndex
+    const row = Math.floor(frameIndex / framesPerRow);
+    const col = frameIndex % framesPerRow;
+    characterImage.style.marginLeft = `-${col * frameWidth}px`;
+    characterImage.style.marginTop = `-${row * frameHeight}px`;
+
+    // Tăng frameIndex lên để chuyển đến khung hình tiếp theo
+    frameIndex = (frameIndex + 1) % (framesPerRow * 2);
+
+    // Thực hiện animateCharacter trong 100ms
+    setTimeout(animateCharacter, 150);
+  }
+}
+
+function startAnimation() {
+  isAnimating = true;
+  animateCharacter();
+}
+
+function stopAnimation() {
+  isAnimating = false;
+}
